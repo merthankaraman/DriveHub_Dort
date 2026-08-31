8c37f5d7400785bacf4d9cf5b92cfc84c922aba1
Merthan KARAMAN <merth4nkaraman@gmail.com>
2026-08-31 15:08:17 +0300

    charging page updated

--- app/src/main/java/com/drivehub/dort/ui/MainActivity.java
+++ app/src/main/java/com/drivehub/dort/ui/MainActivity.java
@@ -1601,7 +1601,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (mTvLeftColumn != null) {
             //if (!Float.isNaN(dcAmpExp) && ((Math.abs(dcAmpExp - 511.50) <= 0.4f) || dcAmpExp == 0f)){
-            if (dcAmpExp == 511.50f || dcAmpExp == 0f){
+            if (!charging){
                 mTvLeftColumn.setVisibility(View.GONE);
             }
             else {
